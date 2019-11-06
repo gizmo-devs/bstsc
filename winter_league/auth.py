261c4aa3ae1716ba10a93016e587acd6dcb88032
Craig Attwood <craigattwood@Craigs-MBP.broadband>
2019-11-06 12:35:22 +0000

    Some auth and login assitance

--- winter_league/auth.py
+++ winter_league/auth.py
@@ -13,7 +13,7 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
-        username = request.form['username']
+        username = request.form['username'].strip().lower()
         password = request.form['password']
         firstname = request.form['firstname']
         surname = request.form['surname']
@@ -44,7 +44,7 @@ def register():
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        username = request.form['username'].strip().lower()
         password = request.form['password']
         db = get_db()
         error = None
@@ -65,6 +65,35 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+@bp.route('/reset_password', methods=('GET', 'POST'))
+def reset_user_pw():
+    if request.method == 'POST':
+        username = request.form['username'].strip().lower()
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is None:
+            error = 'User {} is not registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'UPDATE user SET password=? WHERE username=?',
+                (generate_password_hash(password),username)
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('auth/reset_password.html')
 
 @bp.before_app_request
 def load_logged_in_user():
